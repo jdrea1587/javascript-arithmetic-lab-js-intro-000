978d6b0b2a69b3fc3674220ff1f798c0f1c6cb5e
Joshua Rea <joshdanielrea@gmail.com>
2021-03-03 16:10:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,24 @@ function decrement(n){
    n--;
    return n;
 }
+/*
+describe('makeInt(n)', function() {
+  it('parses n as an integer and returns the parsed integer', function() {
+    expect(makeInt(a.toString())).toEqual(a)
+  })
+
+  it('assumes base 10', function() {
+    expect(makeInt('0x2328')).toEqual(0)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
+  })
+})
+*/
+function makeInt(n){
+  number = parseInt(n,10);
+  return number;
+}
+
+
