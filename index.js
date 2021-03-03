dd6c297157a5672f524b26db5c2016089dcfb692
Joshua Rea <joshdanielrea@gmail.com>
2021-03-03 16:14:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,3 +42,18 @@ function makeInt(n){
   n = parseInt(n,10);
   return n;
 }
+/*
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+  })
+})
+*/
+function preserveDecimal(n){
+  n = parseFloat(n);
+  return n;
+}
